be1fe8bdfd22051fe3ebf2f4247257263aae03a6
Mikael Lind <elemel@elemel.se>
2009-03-29 21:11:03 +0200

    evolve and save image

--- lib/dots/DotsWindow.py
+++ lib/dots/DotsWindow.py
@@ -1,20 +1,51 @@
+from __future__ import division
+
 import os, pyglet, random, sys
+import cPickle as pickle
 import PIL.Image, PIL.ImageChops, PIL.ImageStat
 from pyglet.gl import *
 
 dir_path = os.path.dirname(os.path.abspath(__file__))
 
 class DotsWindow(pyglet.window.Window):
-    def __init__(self, goal):
+    def __init__(self, goal_path):
+        caption = 'Dots: %s' % os.path.split(goal_path)[1]
         pyglet.window.Window.__init__(self, width=256, height=256,
-                                      caption='Dots')
-        self.goal = goal
+                                      caption=caption)
+        self.goal_path = goal_path
+        self.goal = PIL.Image.open(goal_path).convert('RGB')
         glEnable(GL_BLEND)
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
         dot_path = os.path.join(dir_path, 'dot.png')
         dot_image = pyglet.image.load(dot_path)
         self.dot_texture = dot_image.get_texture()
-        self.dots = [self.generate_dot() for _ in xrange(256)]
+        self.dots_path = '%s.dots' % os.path.splitext(self.goal_path)[0]
+        try:
+            dots_file = open(self.dots_path)
+            try:
+                self.dots = pickle.load(dots_file)
+            finally:
+                dots_file.close()
+        except Exception, e:
+            print e
+            self.dots = self.generate_dots()
+        self.screenshot = None
+        self.fitness = None
+        self.best_fitness = None
+        self.best_dots = self.dots
+        pyglet.clock.schedule_interval_soft(self.step, 0.001)
+
+    def step(self, dt):
+        if self.screenshot is not None:
+            diff = PIL.ImageChops.difference(self.screenshot, self.goal)
+            stat = PIL.ImageStat.Stat(diff)
+            rms = stat.rms
+            self.fitness = sum(rms) / len(rms) / 256
+            if self.best_fitness is None or self.fitness < self.best_fitness:
+                self.best_dots = self.dots
+                self.best_fitness = self.fitness
+                print "Fitness: %.9f" % self.best_fitness
+            self.dots = self.mutate_dots(self.best_dots)
 
     def on_draw(self):
         glClearColor(0, 0, 0, 0)
@@ -24,25 +55,14 @@ class DotsWindow(pyglet.window.Window):
                  self.height ** 2 / self.dot_texture.height, 1)
         for dot in self.dots:
             self.draw_dot(dot)
-        screenshot = self.get_screenshot()
-        diff = PIL.ImageChops.difference(screenshot, self.goal)
-        stat = PIL.ImageStat.Stat(diff)
-        print stat.rms
-
-    def generate_dot(self):
-        r = random.random
-        center = r(), r()
-        radius = r()
-        color = r(), r(), r(), r()
-        return center, radius, color
+        self.screenshot = self.get_screenshot()
 
     def draw_dot(self, dot):
-        center, radius, color = dot
-        x, y = center
+        x, y, radius, red, green, blue, alpha = dot
         glPushMatrix()
         glTranslated(x, y, 0)
         glScaled(radius, radius, 1)
-        glColor4d(*color)
+        glColor4d(red, green, blue, alpha)
         glEnable(self.dot_texture.target)
         glBindTexture(self.dot_texture.target, self.dot_texture.id)
         glBegin(GL_QUADS)
@@ -58,13 +78,55 @@ class DotsWindow(pyglet.window.Window):
         glDisable(self.dot_texture.target)
         glPopMatrix()
 
+    def generate_dots(self):
+        return tuple(self.generate_dot() for _ in xrange(256))
+
+    def mutate_dots(self, dots):
+        mutation = random.choice([self.move_dot, self.replace_dot,
+                                  self.mutate_dot])
+        return mutation(dots)
+
+    def move_dot(self, dots):
+        dots = list(dots)
+        i = random.randrange(len(dots))
+        j = random.randrange(len(dots))
+        dot = dots.pop(i)
+        dots.insert(j, dot)
+        return tuple(dots)
+
+    def replace_dot(self, dots):
+        dots = list(dots)
+        i = random.randrange(len(dots))
+        j = random.randrange(len(dots))
+        dots.pop(i)
+        dots.insert(j, self.generate_dot())
+        return tuple(dots)
+
+    def mutate_dot(self, dots):
+        dots = list(dots)
+        i = random.randrange(len(dots))
+        dot = list(dots[i])
+        j = random.randrange(len(dot))
+        dot[j] += random.choice([-1, 1]) * random.random() ** 3
+        dot[j] = max(0, min(dot[j], 1))
+        dots[i] = tuple(dot)
+        return tuple(dots)
+
+    def generate_dot(self):
+        return tuple(random.random() for _ in xrange(7))
+
     def on_close(self):
         self.close()
 
     def on_key_press(self, symbol, modifiers):
         if symbol == pyglet.window.key.ESCAPE:
-            image = self.get_pil_image()
-            image.save('dots.png')
+            dots_file = open(self.dots_path, 'w')
+            try:
+                pickle.dump(self.best_dots, dots_file, pickle.HIGHEST_PROTOCOL)
+            finally:
+                dots_file.close()
+            if self.screenshot is not None:
+                self.screenshot.save(self.dots_path + '.png')
             self.close()
 
     def get_screenshot(self):
--- lib/dots/main.py
+++ lib/dots/main.py
@@ -6,8 +6,7 @@ def main():
     if len(sys.argv) != 2:
         print "Usage: dots <goal>"
         sys.exit(1)
-    goal = PIL.Image.open(sys.argv[1]).convert('RGB')
-    window = DotsWindow(goal)
+    window = DotsWindow(sys.argv[1])
     pyglet.app.run()
 
 if __name__ == '__main__':
