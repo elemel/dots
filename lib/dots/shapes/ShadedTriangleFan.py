e9618fd08717068b9310e616aae3231f0870c7b0
Mikael Lind <elemel@elemel.se>
2009-04-06 21:55:32 +0200

    major rewrite of ShadedTriangleFan

--- lib/dots/shapes/ShadedTriangleFan.py
+++ lib/dots/shapes/ShadedTriangleFan.py
@@ -11,53 +11,59 @@ class ShadedTriangleFan(object):
     # C = rgba
     # V = xya
 
-    def __init__(self, chromosome):
-        xs = chromosome[4:len(chromosome):3]
-        ys = chromosome[5:len(chromosome):3]
-        center_x, center_y = sum(xs) / len(xs), sum(ys) / len(ys)
-        vertex_count = (len(chromosome) - 4) // 3
-        vertices = [chromosome[4 + i * 3:7 + i * 3]
-                    for i in xrange(vertex_count)]
-        def angle(vertex):
-            x, y, a = vertex
-            return atan2(y - center_y, x - center_x)
-        chromosome = chromosome[:4]
-        chromosome.extend(chain(*sorted(vertices, key=angle)))
-        self.chromosome = tuple(chromosome)
+    def __init__(self, color, vertices):
+        self.color = tuple(color)
+        self.vertices = tuple(tuple(v) for v in vertices)
 
     @staticmethod
     def generate(random):
-        x, y = random.random(), random.random()
+        center_x, center_y = random.random(), random.random()
         color = [random.random() for _ in xrange(4)]
         radius = 0.1 * random.random()
-        chromosome = []
-        chromosome.extend(color)
+        vertices = []
         for _ in xrange(7):
             angle = 2 * pi * random.random()
-            chromosome.append(x + radius * cos(angle))
-            chromosome.append(y + radius * sin(angle))
-            chromosome.append(0)
-        return ShadedTriangleFan(chromosome)
+            vertex_x = center_x + radius * cos(angle)
+            vertex_y = center_y + radius * sin(angle)
+            vertices.append((vertex_x, vertex_y, 0))
+        return ShadedTriangleFan(color, vertices)
+
+    @staticmethod
+    def decode(chromosome):
+        chromosome = tuple(chromosome)
+        color = chromosome[:4]
+        vertex_count = (len(chromosome) - 4) // 3
+        vertices = [chromosome[4 + i * 3:7 + i * 3]
+                    for i in xrange(vertex_count)]
+        return ShadedTriangleFan(color, vertices)
+
+    def encode(self):
+        return self.color + tuple(chain(*self.vertices))
 
     def mutate(self, random):
-        chromosome = list(self.chromosome)
+        chromosome = list(self.encode())
         i = random.randrange(len(chromosome))
-        chromosome[i] += random.choice([-1, 1]) * 0.1 * random.random()
+        sigma = random.choice([0.001, 0.01, 0.1])
+        chromosome[i] = random.normalvariate(chromosome[i], sigma)
         chromosome[i] = max(0, min(chromosome[i], 1))
-        return ShadedTriangleFan(chromosome)
+        return ShadedTriangleFan.decode(chromosome)
 
     def draw(self, graphics):
-        r, g, b, a = self.chromosome[:4]
-        xs = self.chromosome[4:len(self.chromosome):3]
-        ys = self.chromosome[5:len(self.chromosome):3]
+        r, g, b, center_a = self.color
+        xs = [x for x, y, a in self.vertices]
+        ys = [y for x, y, a in self.vertices]
+        center_x, center_y = sum(xs) / len(xs), sum(ys) / len(ys)
         glBegin(GL_TRIANGLE_FAN)
-        glColor4d(r, g, b, a / 2)
-        glVertex2d(sum(xs) / len(xs), sum(ys) / len(ys))
-        for i in xrange((len(self.chromosome) - 4) // 3):
-            x, y, a = self.chromosome[4 + i * 3:7 + i * 3]
-            glColor4d(r, g, b, a / 2)
-            glVertex2d(x, y)
-        x, y, a = self.chromosome[4:7]
-        glColor4d(r, g, b, a / 2)
-        glVertex2d(x, y)
+        glColor4d(r, g, b, center_a / 2)
+        glVertex2d(center_x, center_y)
+        def angle(vertex):
+            vertex_x, vertex_y, vertex_a = vertex
+            return atan2(vertex_y - center_y, vertex_x - center_x)
+        vertices = sorted(self.vertices, key=angle)
+        for vertex_x, vertex_y, vertex_a in vertices:
+            glColor4d(r, g, b, vertex_a / 2)
+            glVertex2d(vertex_x, vertex_y)
+        vertex_x, vertex_y, vertex_a = vertices[0]
+        glColor4d(r, g, b, vertex_a / 2)
+        glVertex2d(vertex_x, vertex_y)
         glEnd()
